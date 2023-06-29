156b6ad03d20eb038843d189ed714dd92ca4d765
Nuwan-Wimaladharma <nuwanwd20@gmail.com>
2023-06-29 19:48:28 +0530

    Expose web servive get all courses

--- node-js-back-end/src/api/CourseHttpController.ts
+++ node-js-back-end/src/api/CourseHttpController.ts
@@ -2,3 +2,8 @@ import express from 'express';
 import {datasource} from "../db/dbcp";
 
 export const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const result = await datasource.query('SELECT * FROM course');
+    res.json(result);
+});
