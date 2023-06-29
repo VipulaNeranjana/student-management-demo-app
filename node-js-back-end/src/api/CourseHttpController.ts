54e167f20921bbddebc8d9781a396f018b449642
Nuwan-Wimaladharma <nuwanwd20@gmail.com>
2023-06-29 19:24:47 +0530

    create an app using express and make it to listen port 8080

---
+++ node-js-back-end/src/api/CourseHttpController.ts
@@ -0,0 +1,4 @@
+import express from 'express';
+import {datasource} from "../db/dbcp";
+
+export const router = express.Router();
---
+++ node-js-back-end/src/main.ts
@@ -0,0 +1,10 @@
+import express, {json} from 'express';
+import {router as BookHttpController} from "./api/CourseHttpController";
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+app.use('/api/v1/courses', BookHttpController);
+app.listen(8080, ()=> console.log("Server has been started at 8080"));
