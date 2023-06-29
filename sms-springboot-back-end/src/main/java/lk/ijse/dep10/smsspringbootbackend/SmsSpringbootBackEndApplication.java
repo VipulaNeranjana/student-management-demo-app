84ae9ed1f97240bad1932a09330f67d880ccfe2c
VipulaNeranjana <vipulana17@gmail.com>
2023-06-29 19:08:26 +0530

    initialize project & create crud dao

---
+++ sms-springboot-back-end/src/main/java/lk/ijse/dep10/smsspringbootbackend/SmsSpringbootBackEndApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep10.smsspringbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsSpringbootBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsSpringbootBackEndApplication.class, args);
+	}
+
+}
---
+++ sms-springboot-back-end/src/main/java/lk/ijse/dep10/smsspringbootbackend/entity/SuperEntity.java
@@ -0,0 +1,6 @@
+package lk.ijse.dep10.smsspringbootbackend.entity;
+
+import java.io.Serializable;
+
+public interface SuperEntity extends Serializable {
+}
