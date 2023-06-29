f060c681998d51be8b4bb6faf180fe5df93e8154
Nuwan-Wimaladharma <nuwanwd20@gmail.com>
2023-06-29 20:04:37 +0530

    Expose web service save a course

--- node-js-back-end/src/api/CourseHttpController.ts
+++ node-js-back-end/src/api/CourseHttpController.ts
@@ -3,7 +3,34 @@ import {datasource} from "../db/dbcp";
 
 export const router = express.Router();
 
+type Course = {
+    id: string,
+    description: string,
+    duration: string
+}
+
 router.get('/', async (req, res) => {
     const result = await datasource.query('SELECT * FROM course');
     res.json(result);
+});
+
+router.post('/', async (req, res) => {
+    const course = req.body as Course;
+    if (!course.id.trim() || !course.description.trim() || !course.duration.trim()){
+        res.status(400).send("Invalid data. id, description and duration cannot be empty");
+    }
+    else {
+        try{
+            const result = await datasource.query("INSERT INTO course (id, description, duration) VALUES (?,?,?)",[course.id, course.description, course.duration]);
+            res.status(201).json(course);
+        }
+        catch (err:any){
+            if (err.sqlState === '23000'){
+                res.status(409).send("Failed to save. Id already exists");
+            }
+            else {
+                throw err;
+            }
+        }
+    }
 });
